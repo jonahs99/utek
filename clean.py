b88b7137070e90722c44989f12f5ee0b230b4437
Jonah Simpson <jonahs99@gmail.com>
2018-01-13 12:10:15 -0500

    nothing

--- clean.py
+++ clean.py
@@ -4,25 +4,25 @@ path_in = './competition-package/ptb.train.txt'
 path_cache = './ngram_counts.txt'
 
 # https://stackoverflow.com/questions/16402525/python-read-whitespace-separated-strings-from-file-similar-to-readline
-def read_by_tokens(fileobj):
+def _read_by_tokens(fileobj):
     for line in fileobj:
         for token in line.split():
             yield token
 
-def string_ngrams(string):
+def _string_ngrams(string):
     for start in range(len(string)):
         for end in range(start + 1, min(start + 7, len(string))):
             yield string[start:end]
 
-def parse_ngrams():
+def _parse_ngrams():
     ngrams = {}
     count = 0
     with open(path_in) as file:
-        for token in read_by_tokens(file):
+        for token in _read_by_tokens(file):
             if token == "<unk>" or token == "N":
                 continue
             upper = token.upper()
-            for ngram in string_ngrams(upper):
+            for ngram in _string_ngrams(upper):
                 if ngram in ngrams:
                     ngrams[ngram] += 1
                 else:
@@ -33,12 +33,12 @@ def parse_ngrams():
         return ngrams
     return None
 
-def cache_ngrams(ngrams):
+def _cache_ngrams(ngrams):
     with open(path_cache, 'w') as file:
         for ngram, count in ngrams.items():
             file.write(ngram + ' ' + str(count) + '\n')
 
-def cache_read_ngrams():
+def _cache_read_ngrams():
     ngrams = {}
     try:
         with open(path_cache, 'r') as file:
@@ -50,9 +50,10 @@ def cache_read_ngrams():
         return None
 
 def ngram_count():
-    ngrams = cache_read_ngrams()
+    ngrams = _cache_read_ngrams()
     if ngrams is not None:
         return ngrams
-    ngrams = parse_ngrams()
-    cache_ngrams(ngrams)
-    return ngrams
+    else:
+        ngrams = _parse_ngrams()
+        _cache_ngrams(ngrams)
+        return ngrams
--- jonah.py
+++ jonah.py
@@ -1,4 +1,4 @@
-
+import clean
 
 def letter(i):
     return chr(i + 65)
@@ -8,5 +8,3 @@ def letter_index(l):
 
 def shift_cypher(string, shift):
     return ''.join([ letter( (letter_index(l) + shift) % 26 ) for l in string ])
-
-print(shift_cypher("ABCDZ", 1))
