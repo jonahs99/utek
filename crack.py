4bbc7a99596d3e47154983b97cbf798411724ae6
Jonah Simpson <jonahs99@gmail.com>
2018-01-13 14:47:03 -0500

    started gen model

--- crack.py
+++ crack.py
@@ -1,4 +1,18 @@
-import Encryption
+import cipher
+import genetic
+import random
 
-class GenChromo:
-    
+class GenChromo(genetic.Chromosome):
+    def fitness(self):
+        return cipher.score(self.string)
+    def mutate(self):
+        lstr = list(self.string)
+        lstr[random.randint(0, len(self.string) - 1)] = chr(random.randint(65, 90))
+        return GenChromo(''.join(lstr))
+
+trainer = genetic.Trainer(100, lambda: GenChromo("AAAAAAAAAAAAAAAAAAAA"))
+
+for i in range(100000):
+    w = trainer.iterate()
+    if i % 10000:
+        print(w)
--- genetic.py
+++ genetic.py
@@ -1,20 +1,22 @@
 import random
 
+'''def mutate(self):
+    a = random.randint(0, len(self.string) - 1)
+    b = random.randint(0, len(self.string) - 1)
+
+    lstr = list(self.string)
+    lstr[a] = self.string[b]
+    lstr[b] = self.string[a]
+
+    offspring = Chromosome(''.join(lstr))
+    return offspring
+'''
 class Chromosome:
     def __init__(self, string):
         self.string = string
     def fitness(self):
         pass
-    def mutate(self):
-        a = random.randint(0, len(self.string) - 1)
-        b = random.randint(0, len(self.string) - 1)
-
-        lstr = list(self.string)
-        lstr[a] = self.string[b]
-        lstr[b] = self.string[a]
-
-        offspring = Chromosome(''.join(lstr))
-        return offspring
+    
     def __repr__(self):
         return self.string
 
@@ -37,6 +39,4 @@ class Trainer:
         
         self.pool = winners + offspring
 
-trainer = Trainer(10, lambda: Chromosome("12345678"))
-trainer.iterate()
-print(trainer.pool)
+        return winners[-1]
